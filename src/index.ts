910ad4730d8087be340b161e9715df4cb232e6a5
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-11 22:58:10 +0545

    Added DB connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("It's alive")
+import { createConnection } from 'typeorm'
+
+const main = async () => {
+    try {
+        const connection = await createConnection({
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "password",
+            database: "typeORM"
+        })
+
+        console.log("Successfully connected to Postgres")
+    } catch (e) {
+        console.error(e)
+        throw new Error("Unable to connect to DB")
+    }
+}
+
+main()
