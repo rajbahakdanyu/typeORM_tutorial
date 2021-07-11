b1e9fa54d62856a954d845fd05c9a40b8d251e4d
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-11 23:32:43 +0545

    Created Banker entity

---
+++ src/entities/Banker.ts
@@ -0,0 +1,43 @@
+import {
+    Entity,
+    BaseEntity,
+    Column,
+    PrimaryColumn,
+    CreateDateColumn,
+    UpdateDateColumn
+} from 'typeorm'
+
+@Entity('banker')
+export class Banker extends BaseEntity {
+    @PrimaryColumn()
+    id: number
+
+    @Column()
+    first_name: string
+
+    @Column()
+    last_name: string
+
+    @Column({
+        unique: true
+    })
+    email: string
+
+    @Column({
+        unique: true,
+        length: 10
+    })
+    card_number: string
+
+    @Column({
+        unique: true,
+        length: 10
+    })
+    employee_number: number
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+}
