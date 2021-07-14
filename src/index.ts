15e94b19f063eb1190c2d8bc279e011ba15b0373
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-14 14:53:38 +0545

    Adding routes to app

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import express from 'express'
 import { Client } from './entities/Client'
 import { Banker } from './entities/Banker'
 import { Transaction } from './entities/Transaction'
+import { createClientRouter } from './routes/create_client'
+import { createBankerRouter } from './routes/create_banker'
+import { connectBankerToClientRouter } from './routes/connect_banker_to_client'
+import { createTransactionRouter } from './routes/create_transaction'
+import { deleteClientRouter } from './routes/delete_client'
+import { fetchClientsRouter } from './routes/fetch_clients'
 
 const app = express()
 
@@ -23,6 +29,13 @@ const main = async () => {
         console.log("Successfully connected to Postgres")
 
         app.use(express.json())
+
+        app.use(createClientRouter)
+        app.use(createBankerRouter)
+        app.use(connectBankerToClientRouter)
+        app.use(createTransactionRouter)
+        app.use(deleteClientRouter)
+        app.use(fetchClientsRouter)
 
         app.listen(8080, () => {
             console.log('Listening on port 8080')
